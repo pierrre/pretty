428b5846d1095017e1c0d48046ecef9af6686410
Pierre Durand <pierredurand@gmail.com>
2024-08-13 05:15:33 +0200

    show base type

--- pretty.go
+++ pretty.go
@@ -1607,6 +1607,9 @@ type TypeAndValueWriter struct {
 	// ShowKnownTypes shows known types.
 	// Default: false.
 	ShowKnownTypes bool
+	// ShowBaseType shows the base type.
+	// Default: true.
+	ShowBaseType bool
 }
 
 // NewTypeAndValueWriter creates a new [TypeAndValueWriter] with default values.
@@ -1615,6 +1618,7 @@ func NewTypeAndValueWriter(t, v ValueWriter) *TypeAndValueWriter {
 		Type:           t,
 		Value:          v,
 		ShowKnownTypes: false,
+		ShowBaseType:   true,
 	}
 }
 
@@ -1623,11 +1627,117 @@ func (vw *TypeAndValueWriter) WriteValue(c *Config, w io.Writer, st State, v ref
 	if !st.KnownType || vw.ShowKnownTypes {
 		writeString(w, "[")
 		mustHandle(vw.Type(c, w, st, v))
-		writeString(w, "] ")
+		writeString(w, "]")
+		vw.writeBaseType(w, v)
+		writeString(w, " ")
 	}
 	st.KnownType = true
 	mustHandle(vw.Value(c, w, st, v))
 	return true
+}
+
+func (vw *TypeAndValueWriter) writeBaseType(w io.Writer, v reflect.Value) {
+	if !vw.ShowBaseType {
+		return
+	}
+	typ := v.Type()
+	if typ.PkgPath() == "" {
+		return
+	}
+	baseType := vw.getCachedBaseType(typ)
+	if baseType == nil {
+		return
+	}
+	writeString(w, "(")
+	writeString(w, baseType.String())
+	writeString(w, ")")
+}
+
+var (
+	baseTypeCacheLock sync.Mutex
+	baseTypeCache     = map[reflect.Type]reflect.Type{}
+)
+
+func (vw *TypeAndValueWriter) getCachedBaseType(typ reflect.Type) reflect.Type {
+	baseTypeCacheLock.Lock()
+	defer baseTypeCacheLock.Unlock()
+	baseType, ok := baseTypeCache[typ]
+	if !ok {
+		baseType = vw.getBaseType(typ)
+		baseTypeCache[typ] = baseType
+	}
+	return baseType
+}
+
+//nolint:gocyclo // We need to handle all kinds.
+func (vw *TypeAndValueWriter) getBaseType(typ reflect.Type) reflect.Type {
+	var baseType reflect.Type
+	switch typ.Kind() { //nolint:exhaustive //Some kinds are not handled.
+	case reflect.Bool:
+		baseType = reflect.TypeFor[bool]()
+	case reflect.Int:
+		baseType = reflect.TypeFor[int]()
+	case reflect.Int8:
+		baseType = reflect.TypeFor[int8]()
+	case reflect.Int16:
+		baseType = reflect.TypeFor[int16]()
+	case reflect.Int32:
+		baseType = reflect.TypeFor[int32]()
+	case reflect.Int64:
+		baseType = reflect.TypeFor[int64]()
+	case reflect.Uint:
+		baseType = reflect.TypeFor[uint]()
+	case reflect.Uint8:
+		baseType = reflect.TypeFor[uint8]()
+	case reflect.Uint16:
+		baseType = reflect.TypeFor[uint16]()
+	case reflect.Uint32:
+		baseType = reflect.TypeFor[uint32]()
+	case reflect.Uint64:
+		baseType = reflect.TypeFor[uint64]()
+	case reflect.Uintptr:
+		baseType = reflect.TypeFor[uintptr]()
+	case reflect.Float32:
+		baseType = reflect.TypeFor[float32]()
+	case reflect.Float64:
+		baseType = reflect.TypeFor[float64]()
+	case reflect.Complex64:
+		baseType = reflect.TypeFor[complex64]()
+	case reflect.Complex128:
+		baseType = reflect.TypeFor[complex128]()
+	case reflect.Array:
+		baseType = reflect.ArrayOf(typ.Len(), typ.Elem())
+	case reflect.Chan:
+		baseType = reflect.ChanOf(typ.ChanDir(), typ.Elem())
+	case reflect.Func:
+		in := make([]reflect.Type, typ.NumIn())
+		for i := range in {
+			in[i] = typ.In(i)
+		}
+		out := make([]reflect.Type, typ.NumOut())
+		for i := range out {
+			out[i] = typ.Out(i)
+		}
+		baseType = reflect.FuncOf(in, out, typ.IsVariadic())
+	case reflect.Map:
+		baseType = reflect.MapOf(typ.Key(), typ.Elem())
+	case reflect.Pointer:
+		baseType = reflect.PointerTo(typ.Elem())
+	case reflect.Slice:
+		baseType = reflect.SliceOf(typ.Elem())
+	case reflect.String:
+		baseType = reflect.TypeFor[string]()
+	case reflect.UnsafePointer:
+		baseType = reflect.TypeFor[unsafe.Pointer]()
+	}
+	if baseType == nil {
+		// Unimplemented: invalid, interface, struct.
+		return nil
+	}
+	if baseType == typ {
+		return nil
+	}
+	return baseType
 }
 
 // TypeValueWriter is a [ValueWriter] that writes the type.
--- pretty_test.go
+++ pretty_test.go
@@ -797,6 +797,184 @@ var testCases = []testCase{
 		},
 	},
 	{
+		name: "TypeCustomBool",
+		value: func() any {
+			type myBool bool
+			return myBool(false)
+		}(),
+	},
+	{
+		name: "TypeCustomInt",
+		value: func() any {
+			type myInt int
+			return myInt(0)
+		}(),
+	},
+	{
+		name: "TypeCustomInt8",
+		value: func() any {
+			type myInt8 int8
+			return myInt8(0)
+		}(),
+	},
+	{
+		name: "TypeCustomInt16",
+		value: func() any {
+			type myInt16 int16
+			return myInt16(0)
+		}(),
+	},
+	{
+		name: "TypeCustomInt32",
+		value: func() any {
+			type myInt32 int32
+			return myInt32(0)
+		}(),
+	},
+	{
+		name: "TypeCustomInt64",
+		value: func() any {
+			type myInt64 int64
+			return myInt64(0)
+		}(),
+	},
+	{
+		name: "TypeCustomUint",
+		value: func() any {
+			type myUint uint
+			return myUint(0)
+		}(),
+	},
+	{
+		name: "TypeCustomUint8",
+		value: func() any {
+			type myUint8 uint8
+			return myUint8(0)
+		}(),
+	},
+	{
+		name: "TypeCustomUint16",
+		value: func() any {
+			type myUint16 uint16
+			return myUint16(0)
+		}(),
+	},
+	{
+		name: "TypeCustomUInt32",
+		value: func() any {
+			type myUint32 uint32
+			return myUint32(0)
+		}(),
+	},
+	{
+		name: "TypeCustomUInt64",
+		value: func() any {
+			type myUint64 uint64
+			return myUint64(0)
+		}(),
+	},
+	{
+		name: "TypeCustomUintptr",
+		value: func() any {
+			type myUintptr uintptr
+			return myUintptr(0)
+		}(),
+	},
+	{
+		name: "TypeCustomFloat32",
+		value: func() any {
+			type myFloat32 float32
+			return myFloat32(0)
+		}(),
+	},
+	{
+		name: "TypeCustomFloat64",
+		value: func() any {
+			type myFloat64 float64
+			return myFloat64(0)
+		}(),
+	},
+	{
+		name: "TypeCustomComplex64",
+		value: func() any {
+			type myComplex64 complex64
+			return myComplex64(0)
+		}(),
+	},
+	{
+		name: "TypeCustomComplex128",
+		value: func() any {
+			type myComplex128 complex128
+			return myComplex128(0)
+		}(),
+	},
+	{
+		name: "TypeCustomArray",
+		value: func() any {
+			type myArray [1]int
+			return myArray{}
+		}(),
+	},
+	{
+		name: "TypeCustomChan",
+		value: func() any {
+			type myChan chan int
+			return myChan(nil)
+		}(),
+	},
+	{
+		name: "TypeCustomFunc",
+		value: func() any {
+			type myFunc func(int, string) (int, error)
+			return myFunc(nil)
+		}(),
+	},
+	{
+		name: "TypeCustomMap",
+		value: func() any {
+			type myMap map[int]int
+			return myMap(nil)
+		}(),
+	},
+	{
+		name: "TypeCustomPointer",
+		value: func() any {
+			type myPointer *int
+			return myPointer(nil)
+		}(),
+	},
+	{
+		name: "TypeCustomSlice",
+		value: func() any {
+			type mySlice []int
+			return mySlice(nil)
+		}(),
+	},
+	{
+		name: "TypeCustomString",
+		value: func() any {
+			type myString string
+			return myString("")
+		}(),
+	},
+	{
+		name: "TypeCustomUnsafePointer",
+		value: func() any {
+			type myUnsafePointer unsafe.Pointer
+			return myUnsafePointer(nil)
+		}(),
+	},
+	{
+		name: "ShowBaseTypeDisabled",
+		value: func() any {
+			type myBool bool
+			return myBool(false)
+		}(),
+		configure: func(vw *CommonValueWriter) {
+			vw.TypeAndValue.ShowBaseType = false
+		},
+	},
+	{
 		name:  "TypeDisabled",
 		value: "test",
 		configure: func(vw *CommonValueWriter) {
