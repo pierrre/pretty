2332ab77e8d7eae603f5d776513c794dd8cd53d7
Pierre Durand <pierredurand@gmail.com>
2025-01-16 13:16:46 +0100

    protobuf

---
+++ ext/protobuf/protobuf.go
@@ -0,0 +1,52 @@
+package protobuf
+
+import (
+	"reflect"
+
+	"github.com/pierrre/pretty"
+	"github.com/pierrre/pretty/internal/indent"
+	"github.com/pierrre/pretty/internal/must"
+	"github.com/pierrre/pretty/internal/write"
+	"google.golang.org/protobuf/reflect/protoreflect"
+)
+
+type ValueWriter struct {
+	pretty.ValueWriter
+}
+
+// WriteValue implements [pretty.ValueWriter].
+func (vw *ValueWriter) WriteValue(st *pretty.State, v reflect.Value) bool {
+	if !v.CanInterface() {
+		return false
+	}
+	pm, ok := v.Interface().(protoreflect.ProtoMessage)
+	if !ok {
+		return false
+	}
+	m := pm.ProtoReflect()
+	defer st.SetRestoreKnownType(false)() // We want to show the types of fields and values.
+	write.MustString(st.Writer, "{")
+	fs := m.Descriptor().Fields()
+	l := fs.Len()
+	hasFields := false
+	st.IndentLevel++
+	for i := range l {
+		fd := fs.Get(i)
+		if !hasFields {
+			write.MustString(st.Writer, "\n")
+			hasFields = true
+		}
+		indent.MustWrite(st.Writer, st.IndentString, st.IndentLevel)
+		fd.Name()
+		write.MustString(st.Writer, string(fd.Name()))
+		write.MustString(st.Writer, ": ")
+		must.Handle(vw.ValueWriter.WriteValue(st, reflect.ValueOf(m.Get(fd).Interface())))
+		write.MustString(st.Writer, ",\n")
+	}
+	st.IndentLevel--
+	if hasFields {
+		indent.MustWrite(st.Writer, st.IndentString, st.IndentLevel)
+	}
+	write.MustString(st.Writer, "}")
+	return true
+}
