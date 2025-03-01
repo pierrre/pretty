881177af8d79dea7659b05d136a9e39d33ce1fc6
Pierre Durand <pierredurand@gmail.com>
2025-03-01 21:57:06 +0100

    document TimeValueWriter

--- time.go
+++ time.go
@@ -7,10 +7,16 @@ import (
 	"github.com/pierrre/pretty/internal/write"
 )
 
+// TimeValueWriter is a [ValueWriter] that handles [time.Time] values.
+//
+// It should be created with [NewTimeValueWriter].
 type TimeValueWriter struct {
+	// Format is the format of the time.
+	// Default: [time.RFC3339Nano].
 	Format string
 }
 
+// NewTimeValueWriter creates a new [TimeValueWriter] with default values.
 func NewTimeValueWriter() *TimeValueWriter {
 	return &TimeValueWriter{
 		Format: time.RFC3339Nano,
@@ -19,6 +25,7 @@ func NewTimeValueWriter() *TimeValueWriter {
 
 var timeType = reflect.TypeFor[time.Time]()
 
+// WriteValue implements [ValueWriter].
 func (wv *TimeValueWriter) WriteValue(st *State, v reflect.Value) bool {
 	if v.Type() != timeType {
 		return false
