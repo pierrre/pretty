f1ecd1cb94dc8d8d64cc43e0e347d3d8fb536ef5
Pierre Durand <pierredurand@gmail.com>
2025-10-30 22:48:01 +0100

    make FilterWriter generic
    It allows to access the wrapped ValueWriter directly with the real type.
    
    Also removed conditions in WriteValue().

--- filter.go
+++ filter.go
@@ -7,23 +7,22 @@ import (
 // FilterWriter is a [ValueWriter] that calls the [ValueWriter] if the filter returns true.
 //
 // It should be created with [NewFilterWriter].
-type FilterWriter struct {
-	ValueWriter
+type FilterWriter[VW ValueWriter] struct {
+	ValueWriter VW
+	// Filter filters values.
+	// The value is handled if it returns true or if it is nil.
 	Filter func(v reflect.Value) bool
 }
 
 // NewFilterWriter creates a new [FilterWriter].
-func NewFilterWriter(vw ValueWriter, f func(v reflect.Value) bool) *FilterWriter {
-	return &FilterWriter{
+func NewFilterWriter[VW ValueWriter](vw VW, f func(v reflect.Value) bool) *FilterWriter[VW] {
+	return &FilterWriter[VW]{
 		ValueWriter: vw,
 		Filter:      f,
 	}
 }
 
 // WriteValue implements [ValueWriter].
-func (vw *FilterWriter) WriteValue(st *State, v reflect.Value) bool {
-	if !vw.Filter(v) {
-		return false
-	}
-	return vw.ValueWriter.WriteValue(st, v)
+func (vw *FilterWriter[VW]) WriteValue(st *State, v reflect.Value) bool {
+	return (vw.Filter == nil || vw.Filter(v)) && vw.ValueWriter.WriteValue(st, v)
 }
--- filter_test.go
+++ filter_test.go
@@ -14,27 +14,48 @@ func init() {
 			Name:  "Match",
 			Value: "test",
 			ConfigureWriter: func(vw *CommonWriter) {
-				vw.ValueWriters = []ValueWriter{NewFilterWriter(
-					ValueWriterFunc(func(st *State, v reflect.Value) bool {
-						write.MustString(st.Writer, "aaaa")
-						return true
-					}),
-					func(v reflect.Value) bool {
-						return v.Type() == reflect.TypeFor[string]()
-					})}
+				vw.ValueWriters = []ValueWriter{
+					NewFilterWriter(
+						ValueWriterFunc(func(st *State, v reflect.Value) bool {
+							write.MustString(st.Writer, "aaaa")
+							return true
+						}),
+						func(v reflect.Value) bool {
+							return v.Type() == reflect.TypeFor[string]()
+						},
+					),
+				}
 			},
 		},
 		{
 			Name:  "NoMatch",
 			Value: 123,
 			ConfigureWriter: func(vw *CommonWriter) {
-				vw.ValueWriters = []ValueWriter{NewFilterWriter(
-					ValueWriterFunc(func(st *State, v reflect.Value) bool {
-						panic("should not be called")
-					}),
-					func(v reflect.Value) bool {
-						return v.Type() == reflect.TypeFor[string]()
-					})}
+				vw.ValueWriters = []ValueWriter{
+					NewFilterWriter(
+						ValueWriterFunc(func(st *State, v reflect.Value) bool {
+							panic("should not be called")
+						}),
+						func(v reflect.Value) bool {
+							return v.Type() == reflect.TypeFor[string]()
+						},
+					),
+				}
+			},
+		},
+		{
+			Name:  "Nil",
+			Value: "test",
+			ConfigureWriter: func(vw *CommonWriter) {
+				vw.ValueWriters = []ValueWriter{
+					NewFilterWriter(
+						ValueWriterFunc(func(st *State, v reflect.Value) bool {
+							write.MustString(st.Writer, "aaaa")
+							return true
+						}),
+						nil,
+					),
+				}
 			},
 		},
 	})
